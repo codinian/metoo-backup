2d69a8b97b6b710d1dabf85ecf1488eb739e52e3
codinian <106952772+codinian@users.noreply.github.com>
2023-07-28 01:34:13 +0800

    Support specifying the path of cookies

--- src/main.py
+++ src/main.py
@@ -3,8 +3,11 @@ import sys
 import argparse
 import facebook_scraper as fs
 
-def get_post_content(url):
-  gen = fs.get_posts(post_urls=[url])
+def get_post_content(url, cookies):
+  if cookies != None:
+    gen = fs.get_posts(post_urls=[url], cookies=cookies)
+  else:
+    gen = fs.get_posts(post_urls=[url])
   post = next(gen)
   time_str = post['time'].strftime('%Y-%m-%d')
   return post['username'], time_str, post['text']
@@ -13,9 +16,10 @@ def main():
   parser = argparse.ArgumentParser()
   parser.add_argument('--url', required=True, type=str, help='The URL of Facebook post')
   parser.add_argument('--filename', required=True, type=str, help='Output filename')
+  parser.add_argument('--cookies', type=str, help='Path of cookies.json')
   args = parser.parse_args()
 
-  author, time_dir, content = get_post_content(args.url)
+  author, time_dir, content = get_post_content(args.url, args.cookies)
 
   if not os.path.exists(time_dir):
     os.makedirs(time_dir)
